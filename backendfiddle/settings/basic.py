12a49fcd35096a348127bebba43a2c898cf40753
swozn <s.s.wozny@gmail.com>
2015-10-27 12:34:56 +0100

    fixed the bdd test

--- backendfiddle/settings/basic.py
+++ backendfiddle/settings/basic.py
@@ -121,3 +121,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'serve', 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),os.path.join(BASE_DIR,'components'))
 LOGIN_URL="/login/github/"
+LOGIN_REDIRECT_URL='/'
