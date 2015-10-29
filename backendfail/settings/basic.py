01c178813dd0cf9a639e9ae65a2d51cdce12899f
swozn <s.s.wozny@gmail.com>
2015-10-29 13:33:35 +0100

    celery implementation

--- backendfail/settings/basic.py
+++ backendfail/settings/basic.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from celery import Celery
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
