f9a50d924041a83886f36b258e8ed672af7c0fb6
swozn <s.s.wozny@gmail.com>
2015-10-29 11:36:39 +0100

    celery implementation

--- backendfail/dj/models.py
+++ backendfail/dj/models.py
@@ -34,5 +34,5 @@ class DjangoFiddle(Fiddle):
         return result
 
     def _launch(self):
-        self.port = launch_django(self.hash).get()
+        self.port = launch_django(self.hash).delay().get()
         self.save()
