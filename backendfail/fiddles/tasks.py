cb681bcda6c2b8b57734d8ec346b61ffc101c488
swozn <s.s.wozny@gmail.com>
2015-11-06 00:49:10 +0100

    Refactored code.

--- backendfail/fiddles/tasks.py
+++ backendfail/fiddles/tasks.py
@@ -2,9 +2,54 @@ from celery import shared_task
 from docker.utils import create_host_config
 from docker import Client
 
+api_base = 'unix://var/run/docker.sock'
+
+
+class DoesNotExist(Exception):
+    pass
+
 
 def get_container_by_name(client, name):
-    return [c for c in client.containers(all=True) if "/" + name in c["Names"]][0]
+    """
+    Well it's kind of obvious what this function does.
+
+    :param client: An instance of a docker api client
+    :param name: The name of the container in question
+    :return: If a container by that name exists, return its dict. Otherwise raise
+    """
+    try:
+        return [c for c in client.containers(all=True) if "/" + name in c["Names"]][0]
+    except IndexError:
+        raise DoesNotExist("This container does not exist")
+
+
+def public_port(container):
+    """
+    Gets the public facing port for a given container
+
+    :param container: dict describing a container. Can be obtained from the dockerclient
+    :return: int public port
+    """
+    return container["Ports"][0]["PublicPort"]
+
+
+def build_container_config(fiddle):
+    EXTERNAL_ROOT, INTERNAL_ROOT = '/var/containers/', '/usr/src/app'
+    host_settings = {
+        "port_bindings": {int(fiddle.internal_port): None},
+        "binds"        : [EXTERNAL_ROOT + fiddle.hash + ':' + INTERNAL_ROOT]
+    }
+    host_config = create_host_config(**host_settings)
+    return {
+        "image"      : fiddle.docker_image,
+        "ports"      : [int(fiddle.internal_port)],
+        "volumes"    : [INTERNAL_ROOT],
+        "working_dir": INTERNAL_ROOT,
+        "command"    : fiddle.startup_command,
+        "name"       : fiddle.hash,
+        "host_config": host_config,
+        "detach"     : True
+    }
 
 
 @shared_task
@@ -12,34 +57,13 @@ def launch_container(fiddle):
     """
     This task takes care of launching the docker container.
     """
-    image = str(fiddle.docker_image)
-    container_root, internal_root = '/var/containers/', '/usr/src/app'
-    ports, volumes = [int(fiddle.internal_port)], [internal_root]
-    api = Client(base_url='unix://var/run/docker.sock')
+    api = Client(base_url=api_base)
+    container_config = build_container_config(fiddle)
     try:
-        myc = get_container_by_name(api, fiddle.hash)
-        api.start(myc["Id"])
-        myc = get_container_by_name(api, fiddle.hash)
-        return myc["Ports"][0]["PublicPort"]
-    except IndexError as e:
-        # The IndexError indicated that this container has never been run yet,
-        # so we should create it.
-        host_config = create_host_config(
-            # We bind the internal server port to a high port, let docker take care of it
-            port_bindings={int(fiddle.internal_port): None},
-            # Also mount the skeleton to the container
-            binds=[container_root + fiddle.hash + ':' + internal_root])
-        # Now start this whole shebang
-        container = api.create_container(
-            image=image,
-            ports=ports,
-            volumes=volumes,
-            working_dir=internal_root,
-            command=fiddle.startup_command,
-            name=fiddle.hash,
-            host_config=host_config,
-            detach=True)
+        started_container = get_container_by_name(api, fiddle.hash)
+        api.start(started_container["Id"])
+        return public_port(get_container_by_name(api, fiddle.hash))
+    except DoesNotExist as e:
+        container = api.create_container(**container_config)
         api.start(container["Id"])
-        # We have to get the port from the container now
-        myc = get_container_by_name(api, fiddle.hash)
-        return myc["Ports"][0]["PublicPort"]
+        return public_port(get_container_by_name(api, fiddle.hash))
