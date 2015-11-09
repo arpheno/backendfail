168c2fe7cf58cb4d0579ff2dfb2611a2b2f92812
swozn <s.s.wozny@gmail.com>
2015-11-09 14:44:59 +0100

    Improved the django skeleton.

--- backendfail/dj/skeleton/urls.py
+++ backendfail/dj/skeleton/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
-from app.views import BlogListView, BlogCreateView
+from app.views import BlogListView, BlogCreateView, index
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='app/app.html')),
+    url(r'^hello/?$', index),
     url(r'^blogs/new/?$', BlogCreateView.as_view(), name='blog-create'),
     url(r'^blogs/?$', BlogListView.as_view(), name='blog-list'),
 ]
