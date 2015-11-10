34a989e2d7c79f607bec5e71a4cbcfdcd42169fd
swozn <s.s.wozny@gmail.com>
2015-11-10 23:09:29 +0100

    Improved monitoring

--- etc/gunicorn.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:8000"
-timeout = 30
-workers = multiprocessing.cpu_count() * 2 + 1
-chdir = "/var/www/bf/backendfail"
-# accesslog = "/vagrant/logs/gunicorn-access-log.txt"
-# errorlog = "/vagrant/logs/gunicorn-error-log.txt"
